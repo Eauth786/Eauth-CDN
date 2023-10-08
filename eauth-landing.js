ccb9b929b44e06654bafe263b4ece57497e3da1a
Eauth.us.to <786eauth@gmail.com>
2023-10-08 15:39:41 +0000

    Update eauth-landing.js

--- eauth-landing.js
+++ eauth-landing.js
@@ -17,34 +17,26 @@
       const switchElement = document.getElementById("switch-2");
       const divElement1 = document.getElementById("monthly");
       const divElement2 = document.getElementById("yearly");
-    
-        // Check if there is a saved state in localStorage
-  const savedState = localStorage.getItem("switchState");
-  if (savedState === "checked") {
-    switchElement.checked = true;
-    divElement1.style.display = "none";
-    divElement2.style.display = "block";
-  } else {
-    switchElement.checked = false;
-    divElement1.style.display = "block";
-    divElement2.style.display = "none";
-  }
 
-    // Check if there is a saved state in localStorage
-    switchElement.checked = true;
-    divElement1.style.display = "none";
-    divElement2.style.display = "block";
+      // Check if checkbox is initially checked
+      if (switchElement.checked) {
+        divElement1.style.display = "none";
+        divElement2.style.display = "block";
+      } else {
+        divElement1.style.display = "block";
+        divElement2.style.display = "none";
+      }
+      
+      // Add event listener to checkbox change event
+      switchElement.addEventListener("change", function() {
+        if (this.checked) {
+          divElement1.style.display = "none";
+          divElement2.style.display = "block";
+        } else {
+          divElement1.style.display = "block";
+          divElement2.style.display = "none";
+        }
+      });
 
-  switchElement.addEventListener("change", function() {
-    if (this.checked) {
-      divElement1.style.display = "none";
-      divElement2.style.display = "block";
-      localStorage.setItem("switchState", "checked");
-    } else {
-      divElement1.style.display = "block";
-      divElement2.style.display = "none";
-      localStorage.setItem("switchState", "unchecked");
-    }
-  });
     var currentYear = new Date().getFullYear();
   document.getElementById("year").textContent = currentYear;
